5de4a08b9b431bb1ad3c2bf86e868b56eeda0ea6
Saheed Lasisi <colosian@yahoo.com>
2020-06-24 16:01:20 +0100

    More titles and my top titles

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -15,6 +15,16 @@ public class QueryProcessor {
         else if (query.toLowerCase().contains("The Great Gatsby")) {
             return "F. Scott Fitzgerald";
         }
-        return "";
+        else if (query.toLowerCase().contains("The Catcher in the Rye")) {
+            return "J.D Salinger";
+        }
+        else if (query.toLowerCase().contains("The Stranger")) {
+            return "Albert Camus";
+        }
+        else if (query.toLowerCase().contains("All the King's Men")) {
+            return "Robert Penn Warren";
+        }
+        return "My Top five Books of all time : 1. In Search of Lost Time by Marcel Proust\n2. Ulysses by James Joyce\n3. Don Quixote by Miguel de Cervante"+
+        "\n4. The Great Gatsby by F. Scott Fitzgerald\5. One Hundred Years of Solitude by Gabriel Garcia Marquez";
     }
 }
